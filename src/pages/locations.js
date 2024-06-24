3eeb3a5d42b30bd71f977905c16a0a69c4cd126f
Mujtaba Althuwaynee <mujtabamft@gmail.com>
2024-06-24 17:38:02 -0400

    second  commit

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -3,7 +3,7 @@ import React from 'react'
 const locations = () => {
   return (
     <div>
-    <h1>Locations</h1>
+    <h1>Locations 123</h1>
       <p>Here is a list of our available locations:</p>
     locations</div>
   )

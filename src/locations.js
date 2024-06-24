78dd2481e0c8da59e0d6e44e2a723984100b386d
Mujtaba Althuwaynee <mujtabamft@gmail.com>
2024-06-24 17:34:52 -0400

    first commit

---
+++ src/locations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const locations = () => {
+  return (
+    <div>
+    <h1>Locations</h1>
+      <p>Here is a list of our available locations:</p>
+    locations</div>
+  )
+}
+
+export default locations

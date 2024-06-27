1d14dfc530d3ac48c64d10cbb702268d4a9af526
Mujtaba Althuwaynee <mujtabamft@gmail.com>
2024-06-27 18:35:20 -0400

    third commit

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -1,12 +1,68 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { DrinkContext } from '../../context/drinkContext';
+import { Box, Image, Text, Heading, Grid, GridItem, Card, CardBody, Stack, Divider, CardFooter, ButtonGroup, Button } from '@chakra-ui/react';
 
-const locations = () => {
-  return (
-    <div>
-    <h1>Locations 123</h1>
-      <p>Here is a list of our available locations:</p>
-    locations</div>
-  )
+const Locations = () => {
+const { locations } = useContext(DrinkContext);
+
+return (
+   
+<Box bg="purple.200">
+<header>
+<Box display="flex" justifyContent="space-between" p="10px 20px" bg="gray.100">
+<Text fontSize="2xl">LogoHere</Text>
+<Box display="flex" gap="20px">
+<Text>Menu</Text>
+<Text>Locations</Text>
+<Text>Order Online</Text>
+<Text>Contact Us</Text>
+</Box>
+</Box>
+</header>
+<Box as="main" p="20px">
+<Grid templateColumns="repeat(3, 1fr)" gap={6}>
+{locations.map((location, index) => (
+<GridItem key={index}>
+  <Card maxW='sm'>
+    <CardBody bg ="red.200">
+      {location.image && (
+        <Image
+          src={location.image}
+          alt={`${location.name} location`}
+          borderRadius='lg'
+          objectFit='cover'
+          height='150px'
+          width='100%'
+        />
+      )}
+      <Stack mt='6' spacing='3'>
+        <Heading size='md'>{location.name}</Heading>
+        <Text>
+          Operating Hours: {location.operatingHour}
+        </Text>
+        <Text color='red.600' fontSize='md'>
+          Phone: {location.phoneNumber}
+        </Text>
+      </Stack>
+    </CardBody>
+    <Divider />
+    <CardFooter>
+      <ButtonGroup spacing='2'>
+        <Button variant='solid' colorScheme='red'>
+          View Details
+        </Button>
+        <Button variant='ghost' colorScheme='red'>
+          Contact
+        </Button>
+      </ButtonGroup>
+    </CardFooter>
+  </Card>
+</GridItem>
+))}
+</Grid>
+</Box>
+</Box>
+);
 }
 
-export default locations
+export default Locations;

0b0b8c2a4c375100f70970960790703c120f764d
sj95265 <sj95265@gmail.com>
2024-06-27 17:36:27 -0400

    toast and clear

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Box, Button, Input, Textarea, VStack, HStack } from '@chakra-ui/react';
+import { Box, Button, Input, Textarea, VStack, HStack, useToast } from '@chakra-ui/react';
 
 const Contact = () => {
   const nameRef = useRef(null);
@@ -7,16 +7,32 @@ const Contact = () => {
   const emailRef = useRef(null);
   const messageRef = useRef(null);
 
+  const toast = useToast()
   const handleSubmit = () => {
-    console.log('Name:', nameRef.current.value);
-    console.log('Phone:', phoneRef.current.value);
-    console.log('Email:', emailRef.current.value);
-    console.log('Message:', messageRef.current.value);
+    const name = nameRef.current.value.trim();
+    const phone = phoneRef.current.value.trim();
+    const email = emailRef.current.value.trim();
+    const message = messageRef.current.value.trim();
+
+    if (name && phone && email && message) {
+      nameRef.current.value = '';
+      phoneRef.current.value = '';
+      emailRef.current.value = '';
+      messageRef.current.value = '';
+      toast({
+        description: "Message sent!",
+        status: 'success',
+        position: 'top',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+    return;
   };
 
   return (
     <Box display="flex" justifyContent="center" alignItems="center" bg="gray.50" h="100vh">
-      <VStack spacing={4} padding={6} boxShadow="md" bg="white" borderRadius="md" border="1px" borderColor="gray.200" h="80%">
+      <VStack spacing={4} padding={6} boxShadow="md" bg="white" borderRadius="md" border="1px" borderColor="gray.200" h="60%" w="60%">
         <HStack spacing={4} width="100%">
           <Box width="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
             <label>Name:</label>

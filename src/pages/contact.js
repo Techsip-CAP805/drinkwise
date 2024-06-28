d2668020563c4f45221ebfb27b7eb6d346ca86ca
sj95265 <sj95265@gmail.com>
2024-06-28 13:46:07 -0400

    Responsive Styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Box, Button, Input, Textarea, VStack, HStack, useToast } from '@chakra-ui/react';
+import { Box, Button, Input, Textarea, VStack, HStack, useToast, useBreakpointValue } from '@chakra-ui/react';
 
 const Contact = () => {
   const nameRef = useRef(null);
@@ -7,7 +7,9 @@ const Contact = () => {
   const emailRef = useRef(null);
   const messageRef = useRef(null);
 
-  const toast = useToast()
+  const toast = useToast();
+  const stackDirection = useBreakpointValue({ base: 'column', md: 'row' });
+
   const handleSubmit = () => {
     const name = nameRef.current.value.trim();
     const phone = phoneRef.current.value.trim();
@@ -25,33 +27,51 @@ const Contact = () => {
         position: 'top',
         duration: 3000,
         isClosable: true,
-      })
+      });
+    } else {
+      toast({
+        description: "Please fill in all fields.",
+        status: 'error',
+        position: 'top',
+        duration: 3000,
+        isClosable: true,
+      });
     }
-    return;
   };
 
   return (
     <Box display="flex" justifyContent="center" alignItems="center" bg="gray.50" h="100vh">
-      <VStack spacing={4} padding={6} boxShadow="md" bg="white" borderRadius="md" border="1px" borderColor="gray.200" h="60%" w="60%">
-        <HStack spacing={4} width="100%">
-          <Box width="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
+      <VStack
+        spacing={4}
+        padding={6}
+        boxShadow="md"
+        bg="white"
+        borderRadius="md"
+        border="1px"
+        borderColor="gray.200"
+        h={{ base: "auto", md: "60%" }}
+        w={{ base: "90%", md: "60%" }}>
+        <HStack spacing={4} w="100%" flexDirection={stackDirection}>
+          <Box w="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
             <label>Name:</label>
             <Input type="text" ref={nameRef} />
           </Box>
-          <Box width="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
+          <Box w="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
             <label>Phone:</label>
             <Input type="text" ref={phoneRef} />
           </Box>
-          <Box width="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
+          <Box w="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
             <label>Email:</label>
             <Input type="text" ref={emailRef} />
           </Box>
         </HStack>
-        <Box width="100%" h="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
+        <Box w="100%" h="100%" border="1px" borderColor="gray.200" padding={2} borderRadius="md">
           <label>Message:</label>
-          <Textarea ref={messageRef} h="93%" resize="none" placeholder="Enter your message here..." />
+          <Textarea ref={messageRef} h="90%" resize="none" placeholder="Enter your message here..." />
         </Box>
-        <Button onClick={handleSubmit} colorScheme="blue" width="20%">Send my msg!</Button>
+        <Button onClick={handleSubmit} w={{ base: "100%", md: "20%" }} colorScheme="blue" >
+          Send my msg!
+        </Button>
       </VStack>
     </Box>
   );

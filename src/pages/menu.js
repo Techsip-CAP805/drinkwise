30a998db1fe26796dc0da935425603a8598fce2c
DEELIN WONG <dc.wang547@gmail.com>
2024-07-03 22:09:33 -0400

    main merge

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,8 @@
 import React, {useState, useContext, useEffect} from 'react';
 import {DrinkContext} from '../../context/drinkContext';
-import {Box, Container, Text} from '@chakra-ui/react';
-// import '../styles/globals.css';
+import { Box, Container, Text, HStack, VStack, Flex } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const menu = () => {
 
@@ -11,55 +12,58 @@ const menu = () => {
   console.log(uniqueCategories);
   const [menu, setMenu] = useState({});
 
-  useEffect (() => {
-    //store items by category
+  useEffect(() => {
     const catDrinks = uniqueCategories.reduce((acc, category) => {
-      acc[category] = drinks.filter(item => item.category === category).slice(0,5);
+      acc[category] = drinks.filter(item => item.category === category).slice(0, 5);
       return acc;
     }, {});
-
     setMenu(catDrinks);
   }, []);
 
+  const chunkArray = (array, chunkSize) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
+
+  const chunks = chunkArray(uniqueCategories, 3);
   
-
   return (
-    <Box bg='white.100'>
-    <Container centerContent p={6}>
-    <Text fontSize='lg' fontWeight={700}>Menu</Text>
-    <div className='menuPane'>
-      {uniqueCategories.map((category) => (
-        <div className = "menuCategory" key={category}>
-          <Text fontSize='md' fontWeight={700}>{category}</Text>
-          <table>
-            {menu[category] && menu[category].map((item) => (
-                <tr key={item.drinkId}>
-                  <td>{item.drinkName}</td>
-                  <td>{item.sizeOptions.map(size=> size.size).join(',')}</td>
-                  {/* <td>{item.basePrice.toFixed(2)} CAD</td> */}
-                </tr>
-              ))}
-          </table>
+    <Box bg='gray.200'>
+    <Navbar/>
+    <Container w='100vw' h='100vh' maxH='100vh' maxW='7xl'>
+    <Flex direction='row'  w='100%' h='100%' justify='center' align='center' pb={40}>
+      {/* <Box width={60} h={80} bg='gray.100' borderRadius='2rem' display='flex' direction='row' justifyContent='center' alignItems='center' _hover={{bg:'tomato'}}>
+      <Text>.mp4 is inserted here</Text>
+      </Box> */}
+        <div className='menuPane'>
+        {chunks.map((chunk, index) => (
+          <HStack key={index} spacing='24px' align='flex-start'>
+            {chunk.map((category) => (
+              <VStack key={category} spacing='24px'>
+                <Box className="menuCategory" marginTop={'24px'} padding={'30px'} bg='gray.100' borderRadius='2rem'>
+                  <Text fontSize='md' fontWeight={700}>{category}</Text>
+                  <table>
+                    {menu[category] && menu[category].map((item) => (
+                      <tr key={item.drinkId}>
+                        <td>{item.drinkName}</td>
+                        <td>{item.sizeOptions.map(size => size.size).join(',')}</td>
+                        {/* <td>{item.basePrice.toFixed(2)} CAD</td> */}
+                      </tr>
+                    ))}
+                  </table>
+                </Box>
+              </VStack>
+            ))}
+          </HStack>
+        ))}
         </div>
-      ))}
-    </div>
-    </Container>  
+    </Flex>
+    <Footer/>
+    </Container>
     </Box>
-
-    // <Box bg='green.100'>
-    //   <Container centerContent p={6}>
-    //     <Text fontSize='lg' fontWeight={700}>Drinks</Text>
-    //     {drinks.map((drinks, index)=> (
-    //       <Box bg='blue.100'>
-    //         <div key={index}>
-    //         <h2>{drinks.drinkName}</h2>
-    //         <p>{drinks.category}</p>
-    //         <p>{drinks.basePrice}</p>
-    //         </div>
-    //       </Box>
-    //     ))}
-    //   </Container>
-    // </Box>
   )
 }
 

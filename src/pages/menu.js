d9d1b80676ad3eb354259e6625266cbd4b43c4dc
DEELIN WONG <dc.wang547@gmail.com>
2024-07-03 22:09:33 -0400

    main merge

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,8 +1,8 @@
 import React, {useState, useContext, useEffect} from 'react';
 import {DrinkContext} from '../../context/drinkContext';
 import { Box, Container, Text, HStack, VStack, Flex } from '@chakra-ui/react';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const menu = () => {
 

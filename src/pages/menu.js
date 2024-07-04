b85fa0e7fc744a5fd51c6c0a7852d9eaae9c3dc7
DEELIN WONG <dc.wang547@gmail.com>
2024-07-03 22:09:33 -0400

    main merge

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,9 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useContext } from "react";
 import { useDrinkContext } from "../../context/drinkContext";
 import { Box, Container, Text, Heading, VStack, Grid, GridItem, Flex, Stack, Spacer} from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+=======
+import React, {useState, useContext, useEffect} from 'react';
+import {DrinkContext} from '../../context/drinkContext';
+import { Box, Container, Text, HStack, VStack, Flex } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+>>>>>>> 30a998d (main merge)
 
 const Menu = () => {
   const { drinks } = useDrinkContext();
@@ -11,6 +19,7 @@ const Menu = () => {
   //want to see how many drinks, currently: 28
   console.log(drinks.length);
 
+<<<<<<< HEAD
   // Extract unique categories from drinks
   const categories = Array.from(new Set(drinks.map(drink => drink.category)));
 
@@ -62,6 +71,60 @@ import React from 'react'
 const menu = () => {
   return (
     <div>menu</div>
+=======
+  useEffect(() => {
+    const catDrinks = uniqueCategories.reduce((acc, category) => {
+      acc[category] = drinks.filter(item => item.category === category).slice(0, 5);
+      return acc;
+    }, {});
+    setMenu(catDrinks);
+  }, []);
+
+  const chunkArray = (array, chunkSize) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
+
+  const chunks = chunkArray(uniqueCategories, 3);
+  
+  return (
+    <Box bg='gray.200'>
+    <Navbar/>
+    <Container w='100vw' h='100vh' maxH='100vh' maxW='7xl'>
+    <Flex direction='row'  w='100%' h='100%' justify='center' align='center' pb={40}>
+      {/* <Box width={60} h={80} bg='gray.100' borderRadius='2rem' display='flex' direction='row' justifyContent='center' alignItems='center' _hover={{bg:'tomato'}}>
+      <Text>.mp4 is inserted here</Text>
+      </Box> */}
+        <div className='menuPane'>
+        {chunks.map((chunk, index) => (
+          <HStack key={index} spacing='24px' align='flex-start'>
+            {chunk.map((category) => (
+              <VStack key={category} spacing='24px'>
+                <Box className="menuCategory" marginTop={'24px'} padding={'30px'} bg='gray.100' borderRadius='2rem'>
+                  <Text fontSize='md' fontWeight={700}>{category}</Text>
+                  <table>
+                    {menu[category] && menu[category].map((item) => (
+                      <tr key={item.drinkId}>
+                        <td>{item.drinkName}</td>
+                        <td>{item.sizeOptions.map(size => size.size).join(',')}</td>
+                        {/* <td>{item.basePrice.toFixed(2)} CAD</td> */}
+                      </tr>
+                    ))}
+                  </table>
+                </Box>
+              </VStack>
+            ))}
+          </HStack>
+        ))}
+        </div>
+    </Flex>
+    <Footer/>
+    </Container>
+    </Box>
+>>>>>>> 30a998d (main merge)
   )
 }
 

b2d0225e13e782ec7777782260c603fd4af5f6ab
DEELIN WONG <dc.wang547@gmail.com>
2024-07-14 19:16:36 -0400

    admin portal drinks modal

--- src/pages/admin/[userID]/edit/menu/order.js
+++ src/pages/admin/[userID]/edit/menu/order.js
@@ -1,20 +1,60 @@
 import React, { useState } from 'react';
-import { Box, Container, VStack, Select, Button, SimpleGrid, Card, CardBody, Image, Text, HStack, Input, Heading, Link } from '@chakra-ui/react';
+import { Box, Container, VStack, Select, Button, SimpleGrid, Card, CardBody, Image, Text, HStack, Input, Heading, Link, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input as ChakraInput, useDisclosure } from '@chakra-ui/react';
 import { useDrinkContext } from '../../../../../../context/drinkContext';
-import { AddIcon } from '@chakra-ui/icons';
+import { AddIcon, EditIcon } from '@chakra-ui/icons';
 
 const OrderMenu = () => {
-  const { locations, drinks } = useDrinkContext();
+  const { locations, drinks, setDrinks } = useDrinkContext();
   const [selectedLocation, setSelectedLocation] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
+  const [currentDrink, setCurrentDrink] = useState({ drinkName: '', description: '', basePrice: '', imagePath: '' });
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleLocationChange = (event) => {
     setSelectedLocation(event.target.value);
   };
 
-  const filteredDrinks = drinks.slice(0, 10).filter(drink => 
+  const handleAddDrink = () => {
+    setCurrentDrink({ drinkName: '', description: '', basePrice: '', imagePath: '' });
+    onOpen();
+  };
+
+  const handleEditDrink = (drink) => {
+    setCurrentDrink(drink);
+    onOpen();
+  };
+
+  const handleSaveDrink = () => {
+
+    //check drink already exists
+    if (currentDrink.drinkID) {
+      setDrinks((prevDrinks) =>
+        prevDrinks.map((drink) =>
+          drink.drinkID === currentDrink.drinkID ? currentDrink : drink
+        )
+      );
+    } else {
+      // console.log("PREVIOUS drink array length: " + drinks.length);
+      const newDrink = {
+        ...currentDrink,
+        drinkID: drinks.length +1,
+      };
+      // console.log("NEW DRINK: ", newDrink);
+      setDrinks((prevDrinks) => [...prevDrinks, newDrink]);
+      // console.log("AFTER drink array length: " + drinks.length);
+    }
+
+    onClose();
+  };
+
+  const filteredDrinks = drinks.slice(0, 10).filter(drink =>
     drink.drinkName.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentDrink((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
     <Box bg='#bcc8c3' minH='100vh' display='flex'>
@@ -37,7 +77,7 @@ const OrderMenu = () => {
                 <option key={location.id} value={location.name}>{location.name}</option>
               ))}
             </Select>
-            <Button rightIcon={<AddIcon />} colorScheme='teal'>Add to all</Button>
+            <Button rightIcon={<AddIcon />} colorScheme='teal' onClick={handleAddDrink}>Add to all</Button>
           </HStack>
           <Input
             placeholder='Search drink...'
@@ -53,14 +93,46 @@ const OrderMenu = () => {
                   <Image src={drink.imagePath} alt={drink.drinkName} boxSize='100px' borderRadius='full' bg='tomato' />
                   <Text fontSize='lg' fontWeight='bold'>{drink.drinkName}</Text>
                   <Text>{drink.description}</Text>
-                  <Text>${drink.basePrice.toFixed(2)}</Text>
-                  <Button leftIcon={<AddIcon />} mt={2} colorScheme='teal'>Add Drink</Button>
+                  <Text>${Number(drink.basePrice).toFixed(2)}</Text>
+                  <Button leftIcon={<EditIcon />} mt={2} colorScheme='teal' onClick={() => handleEditDrink(drink)}>Edit Drink</Button>
                 </CardBody>
               </Card>
             ))}
           </SimpleGrid>
         </VStack>
       </Container>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{currentDrink.drinkID ? 'Edit Drink' : 'Add Drink'}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Drink Name</FormLabel>
+              <ChakraInput name='drinkName' value={currentDrink.drinkName} onChange={handleChange} />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <ChakraInput name='description' value={currentDrink.description} onChange={handleChange} />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Price</FormLabel>
+              <ChakraInput name='basePrice' type='number' value={currentDrink.basePrice} onChange={handleChange} />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Image URL</FormLabel>
+              <ChakraInput name='imagePath' value={currentDrink.imagePath} onChange={handleChange} />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='teal' mr={3} onClick={handleSaveDrink}>
+              Save
+            </Button>
+            <Button variant='ghost' onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };

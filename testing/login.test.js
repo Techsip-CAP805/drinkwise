9e642398b0e5a16f9062e57f69c354cbe60f6595
Ven Louie Palicpic <vlpalicpic@myseneca.ca>
2024-07-18 20:33:55 -0400

    ven's unit test - ven

--- testing/login.test.js
+++ testing/login.test.js
@@ -1,38 +1,147 @@
-import { isValid } from '../src/pages/login';
+import React from 'react';
+import {
+    render
+    , screen
+    , fireEvent
+} from '@testing-library/react';
+import LoginPage from '../src/pages/employee/login.js';
+import {
+    ChakraProvider
+    , useToast
+} from '@chakra-ui/react';
+import '@testing-library/jest-dom';
 
-const mockCustomers = [
-    {
-        "_id": {
-            "$oid": "6675d916df0ee7992b11b8c0"
-        },
-        "username": "matthew_king",
-        "customerName": "Matthew King",
-        "emailAddress": "matthew@example.com",
-        "password": "hashedpassword123",
-        "preferredBranch": "downtown",
-        "accountCreationDate": "2024-11-11T13:45:00.000Z",
-        "orders": []
-    },
-    {
-        "_id": {
-            "$oid": "6675d916df0ee7992b11b8c1"
-        },
-        "username": "mia_rodriguez",
-        "customerName": "Mia Rodriguez",
-        "emailAddress": "mia@example.com",
-        "password": "hashedpassword456",
-        "preferredBranch": "richmonhill_yonge",
-        "accountCreationDate": "2024-12-03T10:00:00.000Z",
-        "orders": []
-    }
-];
-
-test('Customer Login isValid()', () => {
-    let result = isValid('test@example.com', 'anypassword', mockCustomers);
-    expect(result).toBeFalsy();
-    result = isValid('matthew@example.com', 'invalidpassword', mockCustomers);
-    expect(result).toBeFalsy();
-    result = isValid('mia@example.com', 'hashedpassword456', mockCustomers);
-    expect(result).toBeTruthy();
+jest.mock('@chakra-ui/react', () => {
+    const originalChakra = jest.requireActual('@chakra-ui/react');
+    return {
+        ...originalChakra,
+        useToast: jest.fn(),
+    };
 });
 
+describe('Employee Login Page', () => {
+    const mockToast = jest.fn();
+    beforeEach(() => {
+        useToast.mockReturnValue(mockToast);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('Sucessful Page Render', () => {
+        render(
+            <ChakraProvider>
+                <LoginPage />
+            </ChakraProvider>
+        );
+        expect(screen.getByText(/Employee Login/i)).toBeInTheDocument();
+    });
+
+    // it('Exclude SideNav on Login Page', () => {
+    //     render(
+    //         <ChakraProvider>
+    //             <LoginPage />
+    //         </ChakraProvider>
+    //     );
+
+    //     // expect(screen.getByText(/Contact Us/i)).toBeInTheDocument();
+    //     expect(screen.getByText(/Edit Menu/i)).toBeInTheDocument();
+    //     // expect(screen.getByText(/Footer/i)).toBeInTheDocument();
+
+    // });
+
+    it('Successful Login Prompt', () => {
+        render(
+            <ChakraProvider>
+                <LoginPage />
+            </ChakraProvider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/Email/i), {
+            target: { value: 'test@example.com' },
+        });
+        fireEvent.change(screen.getByPlaceholderText(/Password/i), {
+            target: { value: 'password' },
+        });
+
+        fireEvent.click(screen.getByRole('button', { name: /Login/i }));
+
+        expect(mockToast).toHaveBeenCalledWith({
+            title: 'Login successful.',
+            description: "You have successfully logged in.",
+            status: 'success',
+            duration: 5000,
+            isClosable: true,
+        });
+    });
+
+    it('Invalid Credentials Prompt', () => {
+        render(
+            <ChakraProvider>
+                <LoginPage />
+            </ChakraProvider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/Email/i), {
+            target: { value: 'invalid@email.com' },
+        });
+        fireEvent.change(screen.getByPlaceholderText(/Password/i), {
+            target: { value: 'wrongpassword' },
+        });
+
+        fireEvent.click(screen.getByRole('button', { name: /Login/i }));
+
+        expect(mockToast).toHaveBeenCalledWith({
+            title: 'Login failed.',
+            description: "Invalid email or password.",
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+        });
+    });
+
+    it('Empty Email Field', () => {
+        render(
+            <ChakraProvider>
+                <LoginPage />
+            </ChakraProvider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/Password/i), {
+            target: { value: 'wrongpassword' },
+        });
+
+        fireEvent.click(screen.getByRole('button', { name: /Login/i }));
+
+        expect(mockToast).toHaveBeenCalledWith({
+            title: 'Login failed.',
+            description: "Email field is required.",
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+        });
+    });
+
+    it('Empty Password Field', () => {
+        render(
+            <ChakraProvider>
+                <LoginPage />
+            </ChakraProvider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/Email/i), {
+            target: { value: 'test@example.com' },
+        });
+
+        fireEvent.click(screen.getByRole('button', { name: /Login/i }));
+
+        expect(mockToast).toHaveBeenCalledWith({
+            title: 'Login failed.',
+            description: "Password field is required.",
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+        });
+    });
+});

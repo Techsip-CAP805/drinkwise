67b323eb38c4bbb77d351831678384e2b7d23c8a
DEELIN WONG <dc.wang547@gmail.com>
2024-07-25 14:14:03 -0400

    customer login touchup

--- src/pages/user/sign-in.js
+++ src/pages/user/sign-in.js
@@ -3,6 +3,8 @@ import { useToast, Box, Button, Container, Heading, Input, VStack, Text, Image, 
 import { Link } from '@chakra-ui/next-js';
 import Router from 'next/router';
 import { signIn } from 'next-auth/react';
+import Navbar from '@/components/Navbar';
+import SmallFooter from '@/components/SmallFooter';
 
 const SignIn = () => {
   const emailRef = useRef();
@@ -40,29 +42,33 @@ const SignIn = () => {
   };
 
   return (
-    <Box bg='#bcc8c3' minH='100vh' display='flex' alignItems='center' justifyContent='center'>
-      <Container maxW='md' bg='white' p={6} borderRadius='md' boxShadow='lg'>
-        <VStack spacing={4} align='center'>
-          <Heading as='h1' size='lg' mb={6} color='#026670'>Login to Account</Heading>
-          <Input
-            placeholder='Email'
-            ref={emailRef}
-            bg='gray.100'
-          />
-          <Input
-            placeholder='Password'
-            type='password'
-            ref={passwordRef}
-            bg='gray.100'
-          />
-          <Button colorScheme='teal' width='full' onClick={handleLogin}>
-            Sign In
-          </Button>
-          <Text>
-            Don&#39;t have an account? <Link href='/user/signUp' color='teal.500'>Sign Up</Link>
-          </Text>
-        </VStack>
-      </Container>
+    <Box bg='#bcc8c3'>
+      <Navbar/>
+      <Box bg='#bcc8c3' minH='100vh' display='flex' alignItems='center' justifyContent='center'>
+        <Container maxW='md' bg='white' p={6} borderRadius='md' boxShadow='lg'>
+          <VStack spacing={4} align='center'>
+            <Heading as='h1' size='lg' mb={6} color='#026670'>Login to Account</Heading>
+            <Input
+              placeholder='Email'
+              ref={emailRef}
+              bg='gray.100'
+            />
+            <Input
+              placeholder='Password'
+              type='password'
+              ref={passwordRef}
+              bg='gray.100'
+            />
+            <Button colorScheme='teal' width='full' onClick={handleLogin}>
+              Sign In
+            </Button>
+            <Text>
+              Don&#39;t have an account? <Link href='/user/signUp' color='teal.500'>Sign Up</Link>
+            </Text>
+          </VStack>
+        </Container>
+      </Box>
+      <SmallFooter/>
     </Box>
   );
 };

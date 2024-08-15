5ebf0a1c73d91e40f125c7fd7adcdac5355e9f79
DEELIN WONG <dc.wang547@gmail.com>
2024-08-15 08:52:39 -0400

    customer profile

--- src/pages/account/[customerID]/orders.js
+++ src/pages/account/[customerID]/orders.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import OrderSideNav from '@/components/OrderSideNav';
-import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import { Box, Text, Heading, VStack, Card, CardBody, Stack, Container, Flex, SimpleGrid, useColorModeValue, Button, HStack, useToast } from '@chakra-ui/react';
+import { Box, Text, Heading, VStack, Card, CardBody, Stack, Container, Flex, Divider, HStack, useColorModeValue } from '@chakra-ui/react';
 
 const CustomerDashboard = () => {
   const [customer, setCustomer] = useState(null);
-  const router = useRouter();
   const { data: session, status } = useSession();
-  const [orders, setOrders] = useState(null);
-  const cardBgColor = useColorModeValue("#a0b2ab", "#283E38");
+  const cardBgColor = useColorModeValue("#f7f7f7", "#1a1a1a"); // Light and dark mode colors
+  const cardTextColor = useColorModeValue("#333", "#fff");
 
   const getCustomerInfo = async () => {
     if (!session?.user?.sub) return;  // Exit if session is not available
@@ -18,8 +16,6 @@ const CustomerDashboard = () => {
       const customerRes = await fetch(`/api/getCustomerByID?id=${session.user.sub}`);
       const customerInfo = await customerRes.json();
       setCustomer(customerInfo);
-      setOrders(customerInfo.orders);
-      console.log(customerInfo);
     } catch (error) {
       console.error('Failed to fetch customer info:', error);
     }
@@ -29,106 +25,72 @@ const CustomerDashboard = () => {
     if (status === 'authenticated') {
       getCustomerInfo();
     }
-
-    console.log("CUSTOMER: ", customer);
   }, [session, status]);
-
-  const renderContent = () => {
-    const { section } = router.query;
-
-    switch (section) {
-      case 'orders':
-        return (
-          <div>
-            <h2>Your Orders</h2>
-            {customer?.orders?.length > 0 ? (
-              <ul>
-                {customer.orders.map((order, index) => (
-                  <li key={index}>{order}</li> // Display order details here
-                ))}
-              </ul>
-            ) : (
-              <p>You have no orders yet.</p>
-            )}
-          </div>
-        );
-      case 'catalog':
-        return <div>Product Catalog</div>;
-      case 'payment':
-        return <div>Payment Information</div>;
-      case 'location':
-        return <div>Change Location</div>;
-      case 'language':
-        return <div>Language Settings</div>;
-      case 'signin':
-        return <div>Sign In</div>;
-      default:
-        return (
-          <div>
-            <h2>Welcome to your dashboard, {customer?.customerName || customer?.username}!</h2>
-            <p><strong>Email Address:</strong> {customer?.emailAddress}</p>
-            <p><strong>Preferred Branch:</strong> {customer?.preferredBranch || "No preferred branch selected"}</p>
-            <p><strong>Account Creation Date:</strong> {new Date(customer?.accountCreationDate).toLocaleDateString()}</p>
-          </div>
-        );
-    }
-  };
 
   const cap = (str) => {
     if (!str) return str; // Handle empty or null strings
     return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+  };
+
+  const sortOrders = (orders) => {
+    return orders.sort((a, b) => {
+      if (a.orderStatus === 'pending' && b.orderStatus !== 'pending') {
+        return -1;
+      }
+      if (a.orderStatus !== 'pending' && b.orderStatus === 'pending') {
+        return 1;
+      }
+      return 0;
+    });
+  };
 
   if (status === 'loading' || !customer) {
     return <div>Loading...</div>;  // Loading state while fetching data
-  };
+  }
+
+  const sortedOrders = sortOrders(customer.orders);
 
   return (
-    <div style={{ display: 'flex', minHeight: '100vh' }}>
+    <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f0f0f0' }}>
       <OrderSideNav />
-      <main style={{ flex: 1, padding: '20px' }}>
-        {renderContent()}
-      </main>
+      <main style={{ flex: 1, padding: '20px', backgroundColor: '#f0f0f0' }}>
+        <Container maxW="container.md" py={10}>
+          <Flex direction="column" justify="center" align="center" w="100%" h="100%">
+            <VStack spacing={6} p={4} w="100%" align="start">
+              <HStack spacing={2} w="100%" align="center" justifyContent="space-between">
+                <Heading as="h2" size="lg" fontWeight="bold" color={cardTextColor}>
+                  My Orders
+                </Heading>
+                <Text as="span" color="gray.500" fontWeight="medium" cursor="pointer" _hover={{ textDecoration: "underline" }}>
+                  Refresh
+                </Text>
+              </HStack>
+              <Divider borderColor="blackAlpha.300" />
 
-      {/* From In Progress */}
-    <Box bg="#bcc8c3">
-      <Box ml="250px">
-        <Container w="100vw" minH="100vh" maxW="70vw" py={10}>
-          <Flex direction="column" justify="center" align="center" w="100%" h="100%" mt={20}>
-            <VStack spacing={6} p={4} w="100%" align="center">
-              <Heading color="white">My Orders</Heading>
-              {orders.length > 0 ? (
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6} w="100%">
-                  {orders.map((order) => (
+              {sortedOrders && sortedOrders.length > 0 ? (
+                <VStack spacing={6} w="100%">
+                  {sortedOrders.map((order) => (
                     <Card
                       key={order._id}
                       borderRadius="lg"
                       overflow="hidden"
                       boxShadow="md"
                       bg={cardBgColor}
-                      maxW="500px"
                       w="100%"
+                      p={4}
                     >
-                      <CardBody p={4}>
+                      <CardBody>
                         <Stack spacing={3}>
-                          <Box p="16px">
-                          <Text color="white">
-                                    Status: {cap(order.orderStatus)}
-                          </Text>
+                          <Box>
+                            <Text color={cardTextColor} fontWeight="bold" fontSize="lg">
+                              Status: {cap(order.orderStatus)}
+                            </Text>
                           </Box>
                           <Box>
                             {order.items.map((item, index) => (
-                              <Box key={index} p={4} borderRadius="md">
-                                <Text color="white">Total Amount: ${(item.basePrice * item.quantity).toFixed(2)}</Text>
-                                <Box mt={3}>
-                                  <Heading size="sm" color="white">Item:</Heading>
-                                  <Text color="white"> 
-                                    {item.drinkName} - Quantity: {item.quantity}
-                                  </Text>
-                                </Box>
-                                {/* <HStack spacing={4} mt={6} justify="center">
-                                  <Button colorScheme="blue" textColor="white" size="sm" w="30%" onClick={() => handleStatusChange(order._id, 'completed')}>Complete</Button>
-                                </HStack> */}
+                              <Box key={index} mt={3}>
+                                <Text color={cardTextColor}>Total Amount: ${(item.basePrice * item.quantity).toFixed(2)}</Text>
+                                <Text color={cardTextColor}>Item: {item.drinkName} - Quantity: {item.quantity}</Text>
                               </Box>
                             ))}
                           </Box>
@@ -136,15 +98,14 @@ const CustomerDashboard = () => {
                       </CardBody>
                     </Card>
                   ))}
-                </SimpleGrid>
+                </VStack>
               ) : (
-                <Text textAlign="center" color="white">No orders found</Text>
+                <Text textAlign="center" color={cardTextColor}>No orders found</Text>
               )}
             </VStack>
           </Flex>
         </Container>
-      </Box>
-    </Box>
+      </main>
     </div>
   );
 };
